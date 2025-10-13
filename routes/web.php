6839eb0e524b1f89e391144c554d3398f78d7f06
Choirudin Emcha <choirudin.emcha@gmail.com>
2025-10-13 22:19:24 +0700

    Add JobController and IsAdmin middleware; update routes for job listings and admin access

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/hello', function () {
+    return "Halo, ini halaman percobaan route!";
+});
+
+Route::get('/jobs', [JobController::class, 'index']);
+
+Route::get('/admin', function () {
+    return "Halaman Admin";
+})->middleware(['auth', 'isAdmin']);
